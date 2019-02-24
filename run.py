73aa2db6b4a19ce8da97fdc390cad3d7787384ba
AjayRajNelapudi <najayraj.16.cse@anits.edu.in>
2019-02-24 19:15:35 +0530

    Multi-user manipulation

--- run.py
+++ run.py
@@ -12,20 +12,48 @@ def capitalize(name):
     name_parts = [part.capitalize() for part in name_parts]
     return ' '.join(name_parts)
 
-voters_file = open('/Users/ajayraj/Documents/SpyryCertificates/sheet1.csv', 'r')
+def sleep_time():
+    time_slots = [3, 7, 12, 6, 2]
+    i = 0
+    while True:
+        yield time_slots[i] * 60
+        i = (i + 1) % len(time_slots)
+
+def get_mr_ms_cursors():
+    mr_cursors = ['Ganesh Patnaik', 'Aditya Adida', 'Aditya Adida', 'Ram Sudeep', 'Aditya Adida', 'Prema Durga Srvan Sai', 'Aditya Adida']
+    ms_cursors = ['Meghana Janapareddy', 'Sravani Gembali', 'Meghana Janapareddy', 'Meghana Janapareddy', 'Prakhya Dasari', 'Meghana Janapareddy', 'Sravani Gembali']
+
+    i = 0
+    while True:
+        yield (mr_cursors[i], ms_cursors[i])
+        i = (i + 1) % len(mr_cursors)
+
+voters_file = open('sheet1.csv', 'r')
 voter_records = list(csv.reader(voters_file))
 voters_file.close()
+
+slot = sleep_time()
+mr_ms_cursors = get_mr_ms_cursors()
+
+points = {'Ram Sudeep': 0, 'Aditya Adida': 0, 'Prema Durga Srvan Sai': 0, 'Ganesh Patnaik': 0,
+          'Meghana Janapareddy': 0, 'Sravani Gembali': 0, 'Prakhya Dasari': 0}
 
 with open('FailLog.txt', 'w') as fail_log:
     vote_caster = voter.Voter()
     for voter_record in voter_records:
         email = make_email(voter_record[1])
         name = capitalize(voter_record[0])
+        mr_cursors, ms_cursors = next(mr_ms_cursors)
         try:
-            vote_caster.vote(email, name)
+            vote_caster.vote(email, name, mr_cursors, ms_cursors)
             vote_caster.submit()
 
-            time.sleep(3)
+            points[mr_cursors] += 1
+            points[ms_cursors] += 1
+            print(name, points)
+
+            delay = next(slot)
+            time.sleep(delay)
         except:
-            print(email, name, 'vote not casted')
+            print(name, 'vote not casted')
             fail_log.write(name + '\n')
--- voter.py
+++ voter.py
@@ -4,12 +4,14 @@ class Voter:
     def __init__(self):
         self.driver = webdriver.Firefox(executable_path=r'/Users/ajayraj/Documents/WebDriver/geckodriver')
 
-    def vote(self, email, name):
+    def vote(self, email, name, mr_cursors, ms_cursors):
         self.driver.get('https://docs.google.com/forms/d/e/1FAIpQLSfyCMOD7nJFfHlScuKuyhUmMc-gpUsK16wKYfGP5kdBcK2Ljg/viewform')
         self.driver.find_element_by_name('emailAddress').send_keys(email)
         self.driver.find_element_by_name('entry.1438007419').send_keys(name)
-        self.driver.find_element_by_css_selector(".quantumWizTogglePaperradioEl.docssharedWizToggleLabeledControl.freebirdThemedRadio.freebirdThemedRadioDarkerDisabled.freebirdFormviewerViewItemsRadioControl[data-value='Ram Sudeep']").click()
-        self.driver.find_element_by_css_selector(".quantumWizTogglePaperradioEl.docssharedWizToggleLabeledControl.freebirdThemedRadio.freebirdThemedRadioDarkerDisabled.freebirdFormviewerViewItemsRadioControl[data-value='Meghana Janapareddy']").click()
+        self.driver.find_element_by_css_selector(
+            ".quantumWizTogglePaperradioEl.docssharedWizToggleLabeledControl.freebirdThemedRadio.freebirdThemedRadioDarkerDisabled.freebirdFormviewerViewItemsRadioControl[data-value='%s']" % (mr_cursors)).click()
+        self.driver.find_element_by_css_selector(
+            ".quantumWizTogglePaperradioEl.docssharedWizToggleLabeledControl.freebirdThemedRadio.freebirdThemedRadioDarkerDisabled.freebirdFormviewerViewItemsRadioControl[data-value='%s']" % (ms_cursors)).click()
 
     def submit(self):
         self.driver.find_element_by_css_selector(".quantumWizButtonPaperbuttonLabel.exportLabel").click()
